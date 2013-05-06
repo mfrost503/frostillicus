58c43bfbdbe555a3ca615b3231c7393017713ec5
Matt Frost <mfrost.design@gmail.com>
2013-05-06 09:02:10 -0400

    initial commit

---
+++ frostillicus/lib/sports/mlb/mlb.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+class mlb:
+    def __init__(self):
+        self.data[]
+    def testOutput()
+        return 'testing'
+
---
+++ sports/mlb.py
@@ -0,0 +1,5 @@
+class Mlb(object):
+    player_id = 1
+    def output(self):
+        return 'hello world'
+
